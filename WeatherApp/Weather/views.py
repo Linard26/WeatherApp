b9d66919a857392c508062c57cefcd9a41d63527
Maks <20052601l@gmail.com>
2024-04-11 01:40:58 +0300

    Данные о погоде в городе

--- WeatherApp/Weather/views.py
+++ WeatherApp/Weather/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid ='6089f83541b6dc77edf5551e12340131'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city ='London'
+    res = requests.get(url.format(city))
+    print(res.text)
     return render(request, 'Weather/index.html')
