a31881fb14078d7d017d20ddac28f5172fb8fdbb
Maks <20052601l@gmail.com>
2024-04-12 20:58:38 +0300

    Шаблон вывода информации о погоде

--- WeatherApp/Weather/views.py
+++ WeatherApp/Weather/views.py
@@ -5,6 +5,14 @@ def index(request):
     appid ='6089f83541b6dc77edf5551e12340131'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
     city ='London'
-    res = requests.get(url.format(city))
-    print(res.text)
-    return render(request, 'Weather/index.html')
+    res = requests.get(url.format(city)).json()
+    city_info={
+        'city':city,
+        'temp': res["main"]["temp"],
+        'icon': res['weather'][0]['icon']
+    }
+    context = {'info':city_info}
+
+
+
+    return render(request, 'Weather/index.html', context)
